805177176888ed61f92c33f0dd910a74e2060177
jluu27 <jasonluu123456@gmail.com>
2026-09-15 14:40:02 -0400

    gambling added

--- slotmachine.py
+++ slotmachine.py
@@ -14,4 +14,14 @@ slots1 = SlotMachine('firstSlot', ['a','b','c','d','e'], 10000)
 
 slots1.deposit(int(input('what do you want to deposit')))
 
-print(slots1.spin())
+result = [slots1.spin(),slots1.spin(),slots1.spin()], \
+    [slots1.spin(),slots1.spin(),slots1.spin()], \
+        [slots1.spin(),slots1.spin(),slots1.spin()]
+winning_row = result[2]
+
+for row in result:
+    print(row)
+if winning_row[1] == winning_row[2] and winning_row[1] == winning_row[3]:
+    print('nice')
+else:
+    print('gamble again')
