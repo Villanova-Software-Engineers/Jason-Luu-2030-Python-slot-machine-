9c116795d6c51086835fb34011daa5d9049853a7
jluu27 <jasonluu123456@gmail.com>
2026-09-17 13:37:04 -0400

    added money system

--- slotmachinewinning.py
+++ slotmachinewinning.py
@@ -8,8 +8,8 @@ class SlotMachine:
         return random.choice(self.symbols)
     def deposit (self, amount):
         self.balance += amount
-        print ('You have', str(amount) + ' dollars')
-slots1 = SlotMachine('firstSlot', ['a','b'], 10000)
+        print ('You have', str(amount) + ' dollars in the machine')
+slots1 = SlotMachine('firstSlot', ['a','b','c','d','e'], 0)
 
 
 def gamblingSequence():
@@ -18,17 +18,25 @@ def gamblingSequence():
     for row in result:
          print(row)
     if winning_row[0] == winning_row[1] and winning_row[0] == winning_row[2]:
-        print('nice')
+        slots1.balance *= 2
+        print('You have doubled your deposit! You now have ' + str(slots1.balance) + ' dollars')
         win = True
         retry = ''
     else: 
-        retry = input('try again y or n')
-    if retry == 'y' or win == False:
+        slots1.balance /= 2
+        print("Don't go bankrupt. You only have " + str(slots1.balance) + " dollars left")
+        retry = input('try again y or n ')
+        win = False
+    if retry == 'n' or retry != 'y' and win == False:
+        print('no keep gambling your about to win big')
+    if retry == 'y' and win == False:
         result = ''
-        for row in result:
-            print(row)
-        gamblingSequence()
-        retry == ''
+        if slots1.balance < 10:
+                print('your cooked bruh only ' + str(slots1.balance) + ' dollars')
+        else:
+            gamblingSequence()
+            retry == ''
+    
 
 def spinning():
     return [ [slots1.spin(),slots1.spin(),slots1.spin()], \
@@ -36,6 +44,6 @@ def spinning():
              [slots1.spin(),slots1.spin(),slots1.spin()] ]
 
 
-slots1.deposit(int(input('what do you want to deposit')))
+slots1.deposit(int(input('what do you want to deposit ')))
 gamblingSequence()
 
