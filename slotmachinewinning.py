038f52555b0e627369c0816260b24c6cff54423c
jluu27 <jasonluu123456@gmail.com>
2026-09-21 11:07:46 -0400

    more gambling

--- slotmachinewinning.py
+++ slotmachinewinning.py
@@ -6,10 +6,11 @@ class SlotMachine:
     def spin(self):
         import random
         return random.choice(self.symbols)
-    def deposit (self, amount):
-        self.balance += amount
-        print ('You have', str(amount) + ' dollars in the machine')
-slots1 = SlotMachine('firstSlot', ['a','b','c','d','e'], 0)
+    def deposit (self, amount, stake):
+        self.balance -= stake
+        print ('You have', str(stake) + ' dollars in the machine')
+        self.stake = stake
+slots1 = SlotMachine('firstSlot', ['a'], 10000)
 
 
 def gamblingSequence():
@@ -18,25 +19,27 @@ def gamblingSequence():
     for row in result:
          print(row)
     if winning_row[0] == winning_row[1] and winning_row[0] == winning_row[2]:
-        slots1.balance *= 2
+        slots1.balance = slots1.balance + slots1.stake * 2 
         print('You have doubled your deposit! You now have ' + str(slots1.balance) + ' dollars')
-        win = True
-        retry = ''
+        restarting()
     else: 
-        slots1.balance /= 2
-        print("Don't go bankrupt. You only have " + str(slots1.balance) + " dollars left")
-        retry = input('try again y or n ')
-        win = False
-    if retry == 'n' or retry != 'y' and win == False:
+        print("You have " + '0' + " dollars left in the machine and need to put in more")
+        print("There is only " + str(slots1.balance) + ' left in your savings')
+        restarting()
+def restarting():
+    retry = input('try again y or n ')
+    if retry == 'n':
         print('no keep gambling your about to win big')
-    if retry == 'y' and win == False:
+    elif retry == 'y': 
         result = ''
-        if slots1.balance < 10:
-                print('your cooked bruh only ' + str(slots1.balance) + ' dollars')
+        slots1.deposit(slots1.balance, int(input('what do you want to deposit ')))
+        if slots1.balance < 0 :
+                print('your cooked bruh ' + str(slots1.balance) + ' dollars')
         else:
             gamblingSequence()
             retry == ''
-    
+    elif retry != 'y' or retry != 'n':
+        retry = input('y or n ')
 
 def spinning():
     return [ [slots1.spin(),slots1.spin(),slots1.spin()], \
@@ -44,6 +47,6 @@ def spinning():
              [slots1.spin(),slots1.spin(),slots1.spin()] ]
 
 
-slots1.deposit(int(input('what do you want to deposit ')))
+slots1.deposit(slots1.balance, int(input('what do you want to deposit. You have ' + str(slots1.balance) + ' left ')))
 gamblingSequence()
 
