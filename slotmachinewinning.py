a4717b68abbbbe25e5a072b1f62185cb047ac13b
jluu27 <jasonluu123456@gmail.com>
2026-09-21 11:15:35 -0400

    bug fix

--- slotmachinewinning.py
+++ slotmachinewinning.py
@@ -16,6 +16,7 @@ slots1 = SlotMachine('firstSlot', ['a'], 10000)
 def gamblingSequence():
     result = spinning()
     winning_row = result[1]
+    fresh_start = False
     for row in result:
          print(row)
     if winning_row[0] == winning_row[1] and winning_row[0] == winning_row[2]:
@@ -28,9 +29,11 @@ def gamblingSequence():
         restarting()
 def restarting():
     retry = input('try again y or n ')
-    if retry == 'n':
+    fresh_start = False
+    if retry == 'n' and not fresh_start:
         print('no keep gambling your about to win big')
-    elif retry == 'y': 
+        restarting()
+    elif retry == 'y' and not fresh_start: 
         result = ''
         slots1.deposit(slots1.balance, int(input('what do you want to deposit ')))
         if slots1.balance < 0 :
@@ -38,15 +41,16 @@ def restarting():
         else:
             gamblingSequence()
             retry == ''
-    elif retry != 'y' or retry != 'n':
+    elif retry != 'y' or retry != 'n' and not fresh_start:
         retry = input('y or n ')
-
+        restarting()
 def spinning():
     return [ [slots1.spin(),slots1.spin(),slots1.spin()], \
              [slots1.spin(),slots1.spin(),slots1.spin()], \
              [slots1.spin(),slots1.spin(),slots1.spin()] ]
-
-
+fresh_start = True
 slots1.deposit(slots1.balance, int(input('what do you want to deposit. You have ' + str(slots1.balance) + ' left ')))
 gamblingSequence()
+
+
 
